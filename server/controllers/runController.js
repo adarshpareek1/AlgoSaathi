1e4b15e035d7e1e3d744cbfc8a44808d8a5fd4ff
adarshpareek1 <154582661+adarshpareek1@users.noreply.github.com>
2026-05-27 23:04:32 +0530

    Update runController.js

--- server/controllers/runController.js
+++ server/controllers/runController.js
@@ -2,34 +2,47 @@ import ax from 'axios';
 import S from '../models/Submission.js';
 
 export const executeCode = async (q, r) => {
-  const p = process.env.PISTON_API_URL || 'https://emkc.org/api/v2/piston/execute';
   const { language: l, source: c, stdin: i } = q.body;
   const u = q.user._id;
   
-  const m = { cpp: 'c++', c: 'c', python: 'python', java: 'java', javascript: 'javascript' };
-  const rt = m[l];
+  const m = { cpp: 54, c: 50, python: 71, java: 62, javascript: 93 };
+  const id = m[l];
 
   try {
-    const d = await ax.post(p, {
-      language: rt,
-      version: "*",
-      files: [{ content: c }],
-      stdin: i || "",
-    });
+    const p = 'https://judge0-ce.p.rapidapi.com/submissions?base64_encoded=false&wait=true';
+    const o = {
+      headers: {
+        'x-rapidapi-key': process.env.JUDGE0_API_KEY,
+        'x-rapidapi-host': 'judge0-ce.p.rapidapi.com',
+        'Content-Type': 'application/json'
+      }
+    };
 
-    const { run: rn } = d.data;
-    const o = rn.stdout || rn.stderr;
-    const e = !!rn.stderr;
+    const d = await ax.post(p, {
+      language_id: id,
+      source_code: c,
+      stdin: i || ""
+    }, o);
+
+    const { stdout: so, stderr: se, compile_output: co } = d.data;
+    const err = se || co;
+    const out = so || err || "";
+    const isErr = !!err;
 
     await S.create({
       user: u,
       language: l,
       code: c,
-      status: e ? 'Error' : 'Success',
-      output: o.substring(0, 500)
+      status: isErr ? 'Error' : 'Success',
+      output: out.substring(0, 500)
     });
 
-    r.json({ output: rn.stdout, error: rn.stderr, code: rn.code });
+    r.json({ 
+      output: so, 
+      error: err, 
+      code: 0 
+    });
+
   } catch (x) {
     console.log('x_err', x.response?.data || x.message);
     r.status(500).json({ message: 'f' });
